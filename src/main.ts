317e2251ba858b4c3f1804fd107773a8036ce4fd
Maccbrainy <iketakumichaelnedum@gmail.com>
2023-06-02 20:28:47 +0100

    app env config settings and using 'ConfigService'

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
+  await app.listen(parseInt(port));
 }
 bootstrap();
