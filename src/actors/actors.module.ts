d24f794e383eafdc25257dcf8a22f6a3629a30c9
Maccbrainy <iketakumichaelnedum@gmail.com>
2023-06-06 06:04:23 +0100

    Sequelize module imports and exports

--- src/actors/actors.module.ts
+++ src/actors/actors.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { ActorsService } from './actors.service';
 import { ActorsController } from './actors.controller';
+import { Actors } from './actors.model';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Actors])],
+  exports: [SequelizeModule],
   controllers: [ActorsController],
-  providers: [ActorsService]
+  providers: [ActorsService],
 })
 export class ActorsModule {}
--- src/genres/genres.module.ts
+++ src/genres/genres.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { Genres } from './genres.model';
 import { GenresService } from './genres.service';
 import { GenresController } from './genres.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Genres])],
+  exports: [SequelizeModule],
   controllers: [GenresController],
-  providers: [GenresService]
+  providers: [GenresService],
 })
 export class GenresModule {}
