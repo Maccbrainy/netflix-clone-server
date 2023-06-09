11f0b3607884c5d0af623cf2e7884913e811e9f9
Maccbrainy <iketakumichaelnedum@gmail.com>
2023-06-09 12:26:28 +0100

    Api payload validation and transformation activation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 import type { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
   app.set('json spaces', 4);
   const port = configService.get('PORT');
   await app.listen(parseInt(port));
