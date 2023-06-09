e79d0c5abcd8628f1ce7e9bca4a3fbaaffedc584
Maccbrainy <iketakumichaelnedum@gmail.com>
2023-06-09 12:31:41 +0100

    Access role services

--- src/access-roles/access-roles.controller.ts
+++ src/access-roles/access-roles.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request } from '@nestjs/common';
 import { AccessRolesService } from './access-roles.service';
+import { CreateAccessRolesDto } from './dto/create-access-roles.dto';
 
-@Controller('access-roles')
+@Controller('api/v1/security-protocol')
 export class AccessRolesController {
   constructor(private readonly accessRolesService: AccessRolesService) {}
+
+  @Post()
+  create(@Body() createAccessRolesDto: CreateAccessRolesDto) {
+    return this.accessRolesService.create(createAccessRolesDto);
+  }
+
+  @Get()
+  findAll(@Request() request: Request) {
+    return this.accessRolesService.findAll(request);
+  }
 }
--- src/access-roles/access-roles.service.ts
+++ src/access-roles/access-roles.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { AccessRoles } from './access-roles.model';
+import { CreateAccessRolesDto } from './dto/create-access-roles.dto';
+import { Op } from 'sequelize';
 
 @Injectable()
-export class AccessRolesService {}
+export class AccessRolesService {
+  constructor(
+    @InjectModel(AccessRoles)
+    private AccessRolesModel: typeof AccessRoles,
+  ) {}
+
+  async create(createAccessRolesDto: CreateAccessRolesDto): Promise<any> {
+    const { roleType, level } = createAccessRolesDto;
+    //check if security protocol "type" or "level" already exist in database;
+    const accessRole: AccessRoles | null = await this.AccessRolesModel.findOne({
+      where: { [Op.or]: [{ level: level }, { roleType: roleType }] },
+    });
+
+    if (accessRole)
+      throw new BadRequestException('Conflicting Security details');
+
+    await this.AccessRolesModel.create({ ...createAccessRolesDto });
+
+    return { message: 'Security Protocol Successfully Created' };
+  }
+
+  async findAll(request: any): Promise<{ accessRoles: AccessRoles[] }> {
+    const accessRoles = await this.AccessRolesModel.findAll({
+      where: {
+        level: {
+          //Get Access role details whose 'levels' is >= to the user request level
+          [Op.gte]: request.user.accessRoleLevel,
+        },
+      },
+      //Get Only details of these attributes
+      attributes: ['id', 'roleType', 'level'],
+      //Arrange by level
+      order: ['level'],
+    });
+    return { accessRoles };
+  }
+}
