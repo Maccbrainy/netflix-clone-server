11f01e663a977f12a23ffd958f4bcab9ae967b69
Maccbrainy <iketakumichaelnedum@gmail.com>
2023-06-24 18:10:10 +0100

    enabling cors, compression and helmet

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
+import compression from 'compression';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import type { NestExpressApplication } from '@nestjs/platform-express';
@@ -8,11 +10,20 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
   app.useGlobalPipes(
+  app.use(helmet());
+  app.enableCors({
+    origin: ['http://127.0.0.1:5173'],
+    methods: ['POST', 'GET', 'PATCH', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+  app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
     }),
   );
+  app.use(compression());
   app.set('json spaces', 4);
+  const configService = app.get(ConfigService);
   const port = configService.get('PORT');
   await app.listen(parseInt(port));
 }
